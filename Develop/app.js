d3c80594940b47d45608832289b8df245ff88052
Mark Mooney <mjmoon15@gmail.com>
2020-07-03 14:28:13 -0400

    changed employee template layout and added pseudocode to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,8 @@
+//import employee classes
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+//npm packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,7 +11,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+//import employee questions
 const managerQuestions = require("./../data/managerQuestions");
 const engineerQuestions = require("./../data/engineerQuestions");
 const internQuestions = require("./../data/internQuestions");
@@ -103,8 +104,8 @@ const getMoreEmployees = (results) => {
             if (res.addAnother) {
                 getEmployeeInfo()
             } else {
-                const renderInfo = render(employees);
-                fs.writeFile(outputPath, renderInfo, (err) => {
+                const renderEmployees = render(employees);
+                fs.writeFile(outputPath, renderEmployees, (err) => {
                     if (err) throw err;
                     console.log('Saved file')
                 })
